a6b14596ea30ee92b64216c3e2012f0db9796590
jsalazar <jsalazar@pe.ibm.com>
2016-08-15 14:33:11 -0500

    2

--- Pru2/src/utils/AppInfo.java
+++ Pru2/src/utils/AppInfo.java
@@ -3,12 +3,19 @@ package utils;
 public class AppInfo {
 
 	public static void Info(String appName, String version, String database, String usuario, String server) {
-		System.out.println("***************************************************************************************");
-		System.out.println("-- Nombre de Aplicación: " + appName);
-		System.out.println("-- Versión de Aplicación: " + version);
-		System.out.println("-- Nombre de la Base de Datos: " + database);
-		System.out.println("-- Usuario de Base de Datos: " + usuario); 
-		System.out.println("-- Hostname de la Base de Datos: "+ server);
-		System.out.println("***************************************************************************************");
+		if(usuario!=null){
+			System.out.println("***************************************************************************************");
+			System.out.println("-- Nombre de Aplicación: " + appName);
+			System.out.println("-- Versión de Aplicación: " + version);
+			System.out.println("-- Nombre de la Base de Datos: " + database);
+			System.out.println("-- Usuario de Base de Datos: " + usuario); 
+			System.out.println("-- Hostname de la Base de Datos: "+ server);
+			System.out.println("***************************************************************************************");
+		} else {
+			System.out.println("***************************************************************************************");
+			System.out.println("-- Error en conexión --");
+			System.out.println("***************************************************************************************");
+		}
+			
 	}
 }
